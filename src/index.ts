f6aca3ec33acb13b83abe3b45858b88a116152dd
queirozmarcondes <renatodequeirozmarcondes@gmail.com>
2025-04-01 21:20:05 -0300

    initial setup

--- src/index.ts
+++ src/index.ts
@@ -12,36 +12,6 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-// Rota para verificação do webhook
-app.get('/webhook', (req: Request, res: Response) => {
-    const VERIFY_TOKEN = process.env.WHATSAPP_VERIFY_TOKEN;
-  
-    const mode = req.query['hub.mode'];
-    const token = req.query['hub.verify_token'];
-    const challenge = req.query['hub.challenge'];
-  
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-    } else {
-      res.sendStatus(403);
-    }
-
-});
-
-// Rota para receber mensagens do webhook
-app.post('/webhook', (req: Request, res: Response) => {
-    const body = req.body;
-  
-    if (body.object === 'whatsapp_business_account') {
-      console.log('Mensagem recebida:', JSON.stringify(body, null, 2));
-      res.sendStatus(200); // WhatsApp exige essa resposta para não reenviar
-    } else {
-      res.sendStatus(404);
-    }
-  });
-
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
